4b5d9f51f810eadeeca2f51c2032d1e0dce8bb64
jainarpit014 <jainarpit014@yahoo.in>
2015-01-19 17:25:26 +0530

    error handling for ajax calls

--- routes/index.js
+++ routes/index.js
@@ -9,37 +9,38 @@ var Twitter = require("twitter");
 
 var client = new Twitter({
 	consumer_key: 'YOUR CONSUMER KEY',
-	consumer_secret: 'YOUR CONSUMER SECRET KEY',
+	consumer_secret: 'YOUR CONSUMER SECRET',
 	access_token_key: 'YOUR ACCESS TOKEN KEY',
 	access_token_secret: 'YOUR ACCESS TOKEN SECRET'
 });
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Twitter Client' });
 });
-/*Search for anything*/
-router.get('/search',function(req,res){
-	client.get('/search/tweets', {q: 'Mumbai'},  function(err, payload){
-  		res.send(payload);
-	});
-});
+
 /*Get user timeline*/
 router.get('/user_timeline',function(req,res){
 	var data = [];
 
 	client.get('statuses/user_timeline',{screen_name:req.query.q,count:10},function(err,payload){
-  		
-  		async.forEach(payload,function(tweet,callback){
+  		if(payload)
+  		{
+	  		async.forEach(payload,function(tweet,callback){
 
-			data.push({text:tweet['text'],name:tweet['user']['name'],screen_name:tweet['user']['screen_name'],profile_image:tweet['user']['profile_image_url']});callback();
-  		},
-  		function(err){
-  			res.header('Cache-Control', 'private, no-cache, no-store, must-revalidate');
-  			res.header('Expires', '-1');
-  			res.header('Pragma', 'no-cache');
-  			res.send(data);
-  		});
+				data.push({text:tweet['text'],name:tweet['user']['name'],screen_name:tweet['user']['screen_name'],profile_image:tweet['user']['profile_image_url']});callback();
+	  		},
+	  		function(err){
+	  			res.header('Cache-Control', 'private, no-cache, no-store, must-revalidate');
+	  			res.header('Expires', '-1');
+	  			res.header('Pragma', 'no-cache');
+	  			res.send(data);
+	  		});
+	  	}
+	  	else
+	  	{
+	  		res.send([]);	
+	  	}
 	});
 });
 /*Get trends in india*/
@@ -60,6 +61,8 @@ router.get('/favorites',function(req,res){
 	
 	client.get('favorites/list',{screen_name:req.query.q,count:10},function(err,payload){
 		var data = [];
+		if(payload)
+		{
 			async.forEach(payload,function(tweet,callback){
 			data.push({text:tweet['text'],name:tweet['user']['name'],screen_name:tweet['user']['screen_name'],profile_image:tweet['user']['profile_image_url']});callback();
   			},
@@ -69,6 +72,10 @@ router.get('/favorites',function(req,res){
   				res.header('Pragma', 'no-cache');
 				res.send(data);
 			});
+		}else
+  		{
+  			res.send([]);
+  		}
 	});
 });
 /*Get user followers*/
@@ -76,6 +83,8 @@ router.get('/followers',function(req,res){
 	
 	client.get('followers/list',{screen_name:req.query.q,count:10},function(err,payload){
 		var data = [];
+		if(payload)
+		{
 			async.forEach(payload['users'],function(follower,callback){
 				data.push({name:follower['name'],screen_name:follower['screen_name'],profile_image:follower['profile_image_url']});callback();
 			},
@@ -85,6 +94,10 @@ router.get('/followers',function(req,res){
   				res.header('Pragma', 'no-cache');
 				res.send(data);
 			});
+		}else
+  		{
+  			res.send([]);
+  		}
 	});
 });
 /*List of users which specified user is following*/
@@ -93,6 +106,8 @@ router.get('/friends',function(req,res){
 	client.get('friends/list',{screen_name:req.query.q,count:10},function(err,payload){
 		
 		var data = [];
+		if(payload)
+		{
 			async.forEach(payload['users'],function(friend,callback){
 				data.push({name:friend['name'],screen_name:friend['screen_name'],profile_image:friend['profile_image_url']});callback();
 			},
@@ -102,6 +117,11 @@ router.get('/friends',function(req,res){
   				res.header('Pragma', 'no-cache');
 				res.send(data);
 			});
+		}
+		else
+  		{
+  			res.send([]);
+  		}
 	});
 });
 
